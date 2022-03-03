969e9f549e79e47cd62650f1faf833516af2a4f4
hedenica <hedenica@hotmail.com>
2022-03-03 16:14:11 -0300

    💎 style(tailwind): initial tailwind css setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 function App () {
   return (
     <div className='App'>
-      <h1>hello world</h1>
+      <h1 className='text-3xl font-bold'>
+        Hello world!
+      </h1>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      blue: '#5457b6',
+      light_blue: '#c3c4ef',
+      dark_blue: '#324152',
+      red: '#ed6468',
+      pink: '#ffb8bb',
+      gray: '#67727e',
+      light_gray: '#eaecf1',
+      lightest_gray: '#f5f6fa',
+      white: '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Rubik'],
+      body: ['Rubik'],
+    },
+  },
+  plugins: [],
+}
